9850de2877cc4a5206ab7190fcdb55cf7b06ce13
Arsalan Jawaid <arsalanjawaid66@gmail.com>
2021-07-25 01:29:04 +0500

    update

---
+++ app.js
@@ -0,0 +1,71 @@
+// //TASK 1
+
+// var itemArray =[
+//     {
+//         name:"juice",
+//         price:"50",
+//         quantity:"3",
+//     },
+//     {
+//         name:"cookie",
+//         price:"30",
+//         quantity:"9",
+//     },
+//     {
+//         name:"shirt",
+//         price:"880",
+//         quantity:"1",
+//     },
+//     {
+//         name:"pen",
+//         price:"100",
+//         quantity:"2",
+//     },
+// ]
+// var item1 = (itemArray[0].price*itemArray[0].quantity);
+// var item2 = (itemArray[1].price*itemArray[1].quantity);
+// var item3 = (itemArray[2].price*itemArray[2].quantity);
+// var item4 = (itemArray[3].price*itemArray[3].quantity);
+
+// var allItem =(item1+item2+item3+item4);
+// console.log(item1);
+// console.log(item2);
+// console.log(item3);
+// console.log(item4);
+// console.log(allItem);
+
+
+ //TASK 2
+// var bioData ={
+//     name:"arsalan",
+//     email:"arsalanjawaid123@gmail.com",
+//     password:"11111",
+//     age:"30",
+//     gender:"male",
+//     city:"karachi",
+//     country:"pakistan"
+// }
+// if("age" in bioData && "country" in bioData){
+//     console.log("age and country found")
+// }else{
+//     console.log("not found")
+// }
+
+
+
+//TASK 3
+
+// function Student(first,last,city,age){
+//     this.firstName = first;
+//     this.lastName = last;
+//     this.city = city;
+//     this.age = age; 
+// }
+// var bio = new Student("Amir","khan","karachi",35)
+// var bio1 = new Student("Atif","aslam","multan",30)
+// var bio2 = new Student("salman","khan","islamabad",40)
+// var bio3 = new Student("Adnan","khan","peshawer",20)
+// console.log(bio.city);
+// console.log(bio1.city);
+// console.log(bio2.city);
+// console.log(bio3.city);
